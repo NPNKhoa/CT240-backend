f1aeaaa892f69838108edb0bcccc9083cc77a2ca
NPNKhoa <khoa.nphucnguyen@gmail.com>
2024-10-19 21:31:25 +0700

    init

---
+++ src/configs/dbConnection.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+export const connectDb = async () => {
+  const primaryUri = process.env.MONGO_URI || '';
+  const fallbackUri = 'mongodb://127.0.0.1:27017/sampleCollection';
+
+  const connect = async (uri) => {
+    try {
+      const conn = await mongoose.connect(uri);
+
+      console.log(`MongoDB connected: ${conn.connection.host}`);
+    } catch (error) {
+      console.log(`Fail to connect to ${uri}. Error: ${error.message}`);
+
+      return false;
+    }
+
+    return true;
+  };
+
+  if (!(await connect(primaryUri))) {
+    console.log('Attempting to connect to the local database...');
+
+    await connect(fallbackUri);
+  }
+};
---
+++ src/server.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import path from 'path';
+
+import { connectDb } from './configs/dbConnection.js';
+
+dotenv.config({ path: `${process.cwd()}/.env` });
+
+connectDb();
+
+const app = express();
+const port = process.env.PORT || 3001;
+const apiVersion = process.env.API_VERSION || 'v1';
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(logger('dev'));
+
+app.use('/uploads', express.static(path.join(path.dirname(''), 'uploads')));
+
+app.use('*', (_, res) => {
+  res.status(404).json({
+    error: 'Oops... Can not found this route!!!',
+  });
+});
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on port ${port}`);
+});
