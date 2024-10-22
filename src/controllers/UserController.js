60b8f52edae5abec0532879f7c5e7992405545db
NPNKhoa <khoa.nphucnguyen@gmail.com>
2024-10-22 20:58:14 +0700

    fix: fix user endpoint

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,6 @@
 import { UserService } from '../services/user.service.js';
+import { handleError } from '../utils/handleError.js';
+import { userUpdateSchema, userIdSchema } from '../validators/userValidator.js';
 
 export class UserController {
   static async getAllUsers(_, res) {
@@ -6,11 +8,16 @@ export class UserController {
       const users = await UserService.getAllUsers();
       res.status(200).json(users);
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      handleError(error, res);
     }
   }
 
   static async getUserById(req, res) {
+    const { error } = userIdSchema.validate(req.params.id);
+    if (error) {
+      return res.status(400).json({ message: error.details[0].message });
+    }
+
     try {
       const user = await UserService.getUserById(req.params.id);
       if (!user) {
@@ -18,25 +25,42 @@ export class UserController {
       }
       res.status(200).json(user);
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      handleError(error, res);
     }
   }
 
   static async updateUser(req, res) {
+    const { error: idError } = userIdSchema.validate(req.params.id);
+
+    if (idError) {
+      return res.status(400).json({ message: idError.details[0].message });
+    }
+
+    const { error: bodyError } = userUpdateSchema.validate(req.body);
+
+    if (bodyError) {
+      return res.status(400).json({ message: bodyError.details[0].message });
+    }
+
     try {
       const user = await UserService.updateUser(req.params.id, req.body);
       res.status(200).json(user);
     } catch (error) {
-      res.status(400).json({ message: error.message });
+      handleError(error, res);
     }
   }
 
   static async deleteUser(req, res) {
+    const { error } = userIdSchema.validate(req.params.id);
+    if (error) {
+      return res.status(400).json({ message: error.details[0].message });
+    }
+
     try {
       await UserService.deleteUser(req.params.id);
       res.status(204).send();
     } catch (error) {
-      res.status(400).json({ message: error.message });
+      handleError(error, res);
     }
   }
 }
