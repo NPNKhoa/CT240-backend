03d845baf14de310646c41ce444c6e3f44d9ae66
NPNKhoa <khoa.nphucnguyen@gmail.com>
2024-10-29 18:41:54 +0700

    fix: add projectId field to phase validator

--- src/services/phase.service.js
+++ src/services/phase.service.js
@@ -1,30 +1,27 @@
 import bcrypt from 'bcrypt';
-import {PhaseDAO} from '../data/PhaseDAO.js';
+import { PhaseDAO } from '../data/PhaseDAO.js';
 export class PhaseService {
-    static async getAllPhase(){
-        const phases = await PhaseDAO.getAllPhase();
-        if (phases === null || (Array.isArray(phases) && phases.length === 0)){
-            throw new NotFoundError('Phase not found');
-        }
-
-        return phases;
+  static async getAllPhase() {
+    const phases = await PhaseDAO.getAllPhase();
+    if (phases === null || (Array.isArray(phases) && phases.length === 0)) {
+      throw new NotFoundError('Phase not found');
     }
 
-    static async getPhaseByName(phaseName){
-        return await PhaseDAO.getPhaseByName(phaseName);
-    }
+    return phases;
+  }
 
-    static async createPhase(phaseData){
-        const Phase = {
-            ...phaseData,
-        };
-        return await PhaseDAO.createPhase(phaseData);
-    }
-    static async updatePhase(phaseId,phaseData){
-        return await PhaseDAO.updatePhase(phaseId,phaseData);
-    }
+  static async getPhaseByName(phaseName) {
+    return await PhaseDAO.getPhaseByName(phaseName);
+  }
 
-    static async deletePhase(phaseId){
-        return await PhaseDAO.deletePhase(phaseId);
-    }
+  static async createPhase(phaseData) {
+    return await PhaseDAO.createPhase(phaseData);
+  }
+  static async updatePhase(phaseId, phaseData) {
+    return await PhaseDAO.updatePhase(phaseId, phaseData);
+  }
+
+  static async deletePhase(phaseId) {
+    return await PhaseDAO.deletePhase(phaseId);
+  }
 }
