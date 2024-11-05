32a8b008c1644e714bb155cda92911de375af531
NPNKhoa <khoa.nphucnguyen@gmail.com>
2024-11-05 13:41:09 +0700

    fix: fix create questionm route

--- src/routes/phaseRoute.js
+++ src/routes/phaseRoute.js
@@ -1,17 +1,17 @@
 import express from 'express';
 import { PhaseController } from '../controllers/PhaseController.js';
-import { auth,isProjectOwner } from '../middlewares/auth.js';
+import { auth, isProjectOwner } from '../middlewares/auth.js';
 
 const router = express.Router();
 
-router.post('/',auth,isProjectOwner,PhaseController.createPhase);
+router.post('/:id', auth, isProjectOwner, PhaseController.createPhase);
 
-router.get('/',auth, PhaseController.getAllPhase);
+router.get('/', auth, PhaseController.getAllPhase);
 
-router.get('/:phaseName',auth, PhaseController.getPhasebyName);
+router.get('/:phaseName', auth, PhaseController.getPhasebyName);
 
-router.put('/:id',auth,isProjectOwner, PhaseController.updatePhase);
+router.put('/:id', auth, isProjectOwner, PhaseController.updatePhase);
 
-router.delete('/:id',auth,isProjectOwner, PhaseController.deletePhase);
+router.delete('/:id', auth, isProjectOwner, PhaseController.deletePhase);
 
 export default router;
